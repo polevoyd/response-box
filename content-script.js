710b93420f3f1754abdde404c929cb72c48d2d90
Dmitry <manxxeta@gmail.com>
2018-08-16 11:04:16 -0700

    workaround clipboard

--- content-script.js
+++ content-script.js
@@ -2,6 +2,7 @@
 //----------------------------------------------------------------
 // keep an object of clicked field in global variable
 let inputFieldClicked;
+let recievedTemplate;
 
 //----------------------------------------------------------------
 // listener for a right mouse click to save input field object reference
@@ -14,14 +15,28 @@ window.addEventListener('contextmenu', function(e)
 // listener for a message to fill a field with template
 browser.runtime.onMessage.addListener(request =>
 {
-  // inputFieldClicked.value += request.template;
-  inputFieldClicked.innerHTML += request.template;
+  /* -------------------------------------------- */
+  // // add template to a innerText
+  // inputFieldClicked.innerText += request.template;
+  /* -------------------------------------------- */
 
+  // keep a recieved template in a global variable
+  recievedTemplate = request.template;
+
+  // working through a copy event
+  document.addEventListener('copy', function(e)
+  {
+    e.clipboardData.setData('text/plain', recievedTemplate);
+    e.preventDefault();
+  });
+
+  // firing copy event
+  document.execCommand('copy');
+
+  // pasting
+  document.execCommand('paste');
 });
 
-
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-
-
