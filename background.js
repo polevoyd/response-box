d924798e908d2a3482741e5120bda0f159d3c29c
Dmitry <manxxeta@gmail.com>
2018-08-16 16:01:19 -0700

    first porting init

--- background.js
+++ background.js
@@ -1,4 +1,13 @@
 
+chrome.storage.local.set({'menu1': 'value1'}, function() {});
+chrome.storage.local.set({'menu2': 'value2'}, function() {});
+chrome.storage.local.set({'menu3': 'value3'}, function() {});
+
+//---------------------------------------------------------------------------
+// keep last selected template here
+let lastMenuClicked = '';
+let lastTemplateSent = '';
+
 //---------------------------------------------------------------------------
 // create a context menus from a storage.local entries
 chrome.storage.local.get((storageEntries) =>
@@ -21,83 +30,26 @@ chrome.browserAction.onClicked.addListener(() =>
   chrome.tabs.create({url: './response-box.html'});
 });
 
-//---------------------------------------------------------------------------
-// keep last selected template here
-let lastMenuClicked = '';
-let lastTemplateSent = '';
-
-//---------------------------------------------------------------------------
-// sending a template to active tab
-// function sendMessageToTabs(tabs)
-// {
-//   for (let tab of tabs)
-//   {
-//     chrome.tabs.sendMessage(
-//       tab.id,
-//       {
-//         template: lastTemplateSent
-//       }
-//     );
-//   }
-// }
-
-// //---------------------------------------------------------------------------
-// // listener on a context menu click
-// chrome.contextMenus.onClicked.addListener((clickedMenu) =>
-// {
-//   // keep last clicked menu in a global variable
-//   lastMenuClicked = clickedMenu.menuItemId;
-
-//   // get a entry we need and keep it global to send
-//   chrome.storage.local.get(lastMenuClicked).then(function(entry)
-//   {
-//     lastTemplateSent = Object.values(entry)[0];
-//   });
-
-//   // select active tab and send template there
-//   chrome.tabs.query(
-//     {
-//       currentWindow: true,
-//       active: true
-//     }
-//   ).then(sendMessageToTabs);
-// });
-
-// //---------------------------------------------------------------------------
-// // on change of local storage recreate a context menus
-// chrome.storage.onChanged.addListener(function()
-// {
-//   // remove all current menu items
-//   chrome.menus.removeAll();
-  
-//   // get all items from a storage
-//   let gettingItem = chrome.storage.local.get();
-//   gettingItem.then(onGot, onError);
-// });
-
-///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-
-
-chrome.storage.local.set({'menu1': 'value1'}, function() {});
-chrome.storage.local.set({'menu2': 'value2'}, function() {});
-chrome.storage.local.set({'menu3': 'value3'}, function() {});
 
 
 //---------------------------------------------------------------------------
 // listener on a context menu click
 chrome.contextMenus.onClicked.addListener((clickedMenu) =>
 {
-  // keep last clicked menu in a global variable
-  lastMenuClicked = clickedMenu.menuItemId;
 
-  // get a entry we need and keep it global to send
-  chrome.storage.local.get(lastMenuClicked, function(entry)
+  console.log('We need to send: ' + clickedMenu.menuItemId);
+
+
+  // get a entry we need to send
+  chrome.storage.local.get(clickedMenu.menuItemId, function(entry) 
   {
     lastTemplateSent = Object.values(entry)[0];
+    console.log('In between: ' + lastTemplateSent);
   });
   
+
+  console.log('We send: ' + lastTemplateSent);
+
   // select active tab and send template there
   chrome.tabs.query(
     {
@@ -117,6 +69,27 @@ chrome.contextMenus.onClicked.addListener((clickedMenu) =>
     }
   );
 });
+
+// //---------------------------------------------------------------------------
+// // on change of local storage recreate a context menus
+// chrome.storage.onChanged.addListener(function()
+// {
+//   // remove all current menu items
+//   chrome.menus.removeAll();
+  
+//   // get all items from a storage
+//   let gettingItem = chrome.storage.local.get();
+//   gettingItem.then(onGot, onError);
+// });
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+
+
+
+
 
 
 
--- content-script.js
+++ content-script.js
@@ -1,13 +1,20 @@
 
-
 //----------------------------------------------------------------
 // listener for a message to fill a field with template
 chrome.runtime.onMessage.addListener(request =>
 {
+  /* -------------------------------------------- */
+  // // add template to a innerText
+  // inputFieldClicked.innerText += request.template;
+  /* -------------------------------------------- */
+
   // working through a copy event
   document.addEventListener('copy', function(e)
   {
     e.clipboardData.setData('text/plain', request.template);
+
+    // console.log('We recieve: ' + request.template);
+    
     e.preventDefault();
   });
 
