7d6b7289110c991af916c79fec70e3d0064f0ef3
Dmitry <manxxeta@gmail.com>
2018-08-17 10:10:04 -0700

    chrome mvp

--- content-script.js
+++ content-script.js
@@ -1,49 +1,31 @@
-
+//----------------------------------------------------------------
 // object clicked
 let clickedTextarea;
 
-// to capture object
+// capture object and keep global
 document.addEventListener('contextmenu', function(event)
 {
-  console.log(event.target);
   clickedTextarea = event.target;
 });
-
 
 //----------------------------------------------------------------
 // listener for a message to fill a field with template
 chrome.runtime.onMessage.addListener(request =>
 {
-  /* -------------------------------------------- */
-  // // add template to a innerText
-  // inputFieldClicked.innerText += request.template;
-  /* -------------------------------------------- */
-
   // working through a copy event
-  document.addEventListener('copy', function(e)
+  clickedTextarea.addEventListener('copy', function(e)
   {
     e.clipboardData.setData('text/plain', request.template);
-
-    console.log('We recieve: ' + request.template);
-    
     e.preventDefault();
   });
 
   // firing copy event
   document.execCommand('copy');
 
-  // focus on area
+  // click, paste, focus
+  clickedTextarea.click();
+  document.execCommand('paste');
   clickedTextarea.focus();
-
-  clickedTextarea.innerHTML = request.template;
-  // clickedTextarea.innerText = request.template;
-
-  // pasting
-  // document.execCommand('paste');
-
-
-
-
 });
 
 /////////////////////////////////////////////////////////////////////////////
