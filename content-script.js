08dbd4f50d81a780f7762fc1f2d8d8eb12865c86
Dmitry <manxxeta@gmail.com>
2018-08-17 10:13:45 -0700

    chrome mvp

--- content-script.js
+++ content-script.js
@@ -2,6 +2,7 @@
 // object clicked
 let clickedTextarea;
 
+//----------------------------------------------------------------
 // capture object and keep global
 document.addEventListener('contextmenu', function(event)
 {
@@ -12,17 +13,14 @@ document.addEventListener('contextmenu', function(event)
 // listener for a message to fill a field with template
 chrome.runtime.onMessage.addListener(request =>
 {
-  // working through a copy event
   clickedTextarea.addEventListener('copy', function(e)
   {
     e.clipboardData.setData('text/plain', request.template);
     e.preventDefault();
   });
 
-  // firing copy event
   document.execCommand('copy');
 
-  // click, paste, focus
   clickedTextarea.click();
   document.execCommand('paste');
   clickedTextarea.focus();
